ef21d9b67d136b3a43d9f5226ffe911a588c5b6b
jrsantos1 <jhonatanjonh@outlook.com>
2023-03-26 19:02:58 -0300

    config connection factory

--- src/main/java/br/com/projetopi/redesocial/HelloServlet.java
+++ src/main/java/br/com/projetopi/redesocial/HelloServlet.java
@@ -1,7 +1,10 @@
 package br.com.projetopi.redesocial;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import br.com.projetopi.redesocial.repository.ConnectionFactory;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -16,13 +19,17 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        try(Connection connectionFactory = ConnectionFactory.getConnection();) {
+            System.out.println("Deu certo");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
---
+++ src/main/java/br/com/projetopi/redesocial/repository/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.projetopi.redesocial.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/rede_social", "root", "root123");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao realizar a conexão");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver de conexão não encontrado");
+        }
+    }
+}
