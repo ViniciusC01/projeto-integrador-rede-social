397073761f06c5e1fd0dedc57e18f36b2694c5c2
jrsantos1 <jhonatanjonh@outlook.com>
2023-04-01 01:49:22 -0300

    crud conta and instituicao, add auto initi db

--- src/main/java/br/com/projetopi/redesocial/dao/ContaDao.java
+++ src/main/java/br/com/projetopi/redesocial/dao/ContaDao.java
@@ -4,6 +4,7 @@ import br.com.projetopi.redesocial.model.Conta;
 import br.com.projetopi.redesocial.repository.ConnectionFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class ContaDao {
 
@@ -32,18 +33,46 @@ public class ContaDao {
             ps.setInt(7, conta.getCurso_id());
             ps.setInt(8, conta.getFoto_id());
             ps.setInt(9, conta.getTurma_id());
-
             ps.execute();
+
         }catch (SQLException e){
             e.printStackTrace();
         }
-
     }
-    public void update(int id){
-
+    public boolean update(Conta conta){
+        String sqlQuery = """
+            update conta 
+            set 
+                cpf = ?,
+                nome = ?,
+                data_nascimento = ?,
+                sobre = ?,
+                usuario_id = ?,
+                instituiacao_id = ?,
+                curso_id = ?,
+                foto_id = ?,
+                turma_id = ?
+            where id  = ?
+        """;
+        try(PreparedStatement ps = conexao.prepareStatement(sqlQuery)) {
+            ps.setString(1, conta.getCpf());
+            ps.setString(2, conta.getNome());
+            ps.setDate(3, conta.getData_nascimento());
+            ps.setString(4, conta.getSobre());
+            ps.setInt(5, conta.getUsuario_id());
+            ps.setInt(6, conta.getInstituiacao_id());
+            ps.setInt(7, conta.getCurso_id());
+            ps.setInt(8, conta.getFoto_id());
+            ps.setInt(9, conta.getTurma_id());
+            ps.setInt(10, conta.getId());
+        }catch (SQLException e){
+            System.out.println("Ocorreu o erro " + e);;
+            return false;
+        }
+        return true;
     }
-    public boolean remove(){
-        String sqlQuery = "delete from conta where id == ?";
+    public boolean remove(int id){
+        String sqlQuery = "delete from conta where id == "+id;
         try(Statement st = conexao.createStatement()){
             st.execute(sqlQuery);
         }catch (SQLException e){
@@ -52,10 +81,33 @@ public class ContaDao {
         return true;
     }
 
-    public void findAll(int qtd_elementos, int pagina){
+    public ArrayList<Conta> findAllPageable(int qtd_elementos, int num_inicio){
 
-        String sqlQuery = "select * from conta where limit ?"; //TODO
+        ArrayList<Conta> contas = new ArrayList<>();
 
+        String sqlQuery = "SELECT * FROM conta LIMIT ? OFFSET ?;"; //TODO
+        try(PreparedStatement ps = conexao.prepareStatement(sqlQuery)) {
+            ps.setInt(1, qtd_elementos);
+            ps.setInt(2, num_inicio);
+            ResultSet result = ps.executeQuery();
+
+            while(result.next()){
+                Conta conta = new Conta();
+                conta.setCpf(result.getString("cpf"));
+                conta.setNome(result.getString("nome"));
+                conta.setData_nascimento(result.getDate("data_nascimento"));
+                conta.setSobre(result.getString("sobre"));
+                conta.setUsuario_id(result.getInt("usuario_id"));
+                conta.setInstituiacao_id(result.getInt("instituiacao_id"));
+                conta.setCurso_id(result.getInt("curso_id"));
+                conta.setFoto_id(result.getInt("foto_id"));
+                conta.setTurma_id(result.getInt("turma_id"));
+                contas.add(conta);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return contas;
     }
 
     public Conta findById(int id){
@@ -79,11 +131,9 @@ public class ContaDao {
                 conta.setFoto_id(result.getInt("foto_id"));
                 conta.setTurma_id(result.getInt("turma_id"));
             }
-
         }catch (SQLException e){
             e.printStackTrace();
         }
-
         return conta;
 
     }
--- src/main/java/br/com/projetopi/redesocial/dao/InstituicaoDao.java
+++ src/main/java/br/com/projetopi/redesocial/dao/InstituicaoDao.java
@@ -1,12 +1,10 @@
 package br.com.projetopi.redesocial.dao;
 
+import br.com.projetopi.redesocial.model.Conta;
 import br.com.projetopi.redesocial.model.Instituicao;
 import br.com.projetopi.redesocial.repository.ConnectionFactory;
 
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class InstituicaoDao {
@@ -42,19 +40,63 @@ public class InstituicaoDao {
         return true;
     }
 
-    public Instituicao update(){
+    public boolean update(Instituicao instituicao){
 
-        //TODO
-
-        return null;
+        String sqlQuery = """
+            update instituicao 
+            set 
+                nome = ?
+            where id  = ?
+        """;
+        try(PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setString(1, instituicao.getNome());
+            ps.setInt(2, instituicao.getId());
+        }catch (SQLException e){
+            System.out.println("Ocorreu o erro " + e);;
+            return false;
+        }
+        return true;
     }
 
-    public ArrayList<Instituicao> findAllPageble(){
-        return null; // TODO
+    public Instituicao findById(int id){
+
+        ResultSet result;
+        Instituicao instituicao = new Instituicao();
+
+        String sqlQuery = "select * from conta where id == ?";
+        try(PreparedStatement ps = con.prepareStatement(sqlQuery)){
+            ps.setInt(1, id);
+            result = ps.executeQuery();
+            while(result.next()){
+                instituicao.setNome(result.getString("nome"));
+                instituicao.setId(result.getInt("id"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return instituicao;
     }
 
-    public Instituicao find(){
-        return null; // TODO
+    public ArrayList<Instituicao> findAllPageble(int qtd_elementos, int num_inicio){
+        ArrayList<Instituicao> instituicoes = new ArrayList<>();
+
+        String sqlQuery = "SELECT * FROM instituicao LIMIT ? OFFSET ?;"; //TODO
+        try(PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setInt(1, qtd_elementos);
+            ps.setInt(2, num_inicio);
+            ResultSet result = ps.executeQuery();
+
+            while(result.next()){
+                Instituicao instituicao = new Instituicao();
+                instituicao.setNome(result.getString("nome"));
+                instituicao.setId(result.getInt("id"));
+                instituicoes.add(instituicao);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return instituicoes;
     }
+
 
 }
