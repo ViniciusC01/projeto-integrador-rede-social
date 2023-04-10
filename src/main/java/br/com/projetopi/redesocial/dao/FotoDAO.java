0d8a6adb7da8e289f0d84196303e421895bc25af
ViniciusC01 <viniciuscarvalho134@gmail.com>
2023-04-10 02:01:25 -0300

    FotoDAO - atualizacao

--- src/main/java/br/com/projetopi/redesocial/dao/FotoDAO.java
+++ src/main/java/br/com/projetopi/redesocial/dao/FotoDAO.java
@@ -3,11 +3,13 @@ package br.com.projetopi.redesocial.dao;
 import br.com.projetopi.redesocial.model.Foto;
 import br.com.projetopi.redesocial.repository.ConnectionFactory;
 
+import javax.servlet.http.HttpServlet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class FotoDAO {
+public class FotoDAO extends HttpServlet {
 
     Connection conexao;
 
@@ -19,20 +21,24 @@ public class FotoDAO {
         }
     }
 
-    void toadd(Foto foto) {
-        String sql = "insert into foto (id, cd_foto) values (?,?)";
+    public void toadd(Foto foto) {
+        String save = "insert into foto (id, cd_foto) values (?,?)";
+        try (PreparedStatement statement = conexao.prepareStatement(save)) {
 
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            stmt.setInt(1, foto.getId());
-            stmt.setLong(2, foto.getCd_foto());
-            stmt.execute();
+            try {
+                statement.setInt(1, foto.getId());
+                statement.setBytes(2, foto.getCd_foto());
+                statement.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
-    boolean remove(Foto foto) {
-        String sql = "DELETE FROM foto WHERE =?";
+    public boolean remove(Foto foto) {
+        String sql = "DELETE FROM foto WHERE id =?";
         try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setInt(1, foto.getId());
             stmt.execute();
@@ -44,10 +50,10 @@ public class FotoDAO {
         return true;
     }
 
-    boolean update(Foto foto) {
-        String sql = "UPDATE FOTO SET cd_foto = ? WHERE =? ";
-        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            stmt.setLong(1, foto.getId());
+    public boolean update(Foto foto) {
+        String update = "UPDATE foto SET cd_foto = ? WHERE id =? ";
+        try (PreparedStatement stmt = conexao.prepareStatement(update)) {
+            stmt.setBytes(1, foto.getCd_foto());
             stmt.setInt(2, foto.getId());
             stmt.execute();
         } catch (SQLException e) {
@@ -55,6 +61,32 @@ public class FotoDAO {
             return false;
         }
         return true;
+    }
+
+    public String consultaPorId(int id) {
+        String select = "SELECT * FROM foto WHERE id =?";
+        StringBuilder sb = new StringBuilder();
+        ResultSet resultado;
+        String s = null;
+        Foto ft = new Foto();
+
+        try (PreparedStatement stmt = conexao.prepareStatement(select)) {
+            stmt.setInt(1, id);
+            resultado = stmt.executeQuery();
+
+            resultado.next();
+            ft.setId(resultado.getInt("id"));
+//            ft.setCd_foto(resultado.getLong("cd_foto"));
+
+            s = String.valueOf(resultado.getInt("id"));
+//                        + String.valueOf(resultado.getBigDecimal("cd_foto")));
+
+            return s;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(s);
+        return s;
     }
 
 
