c52cdeb0f2ec1719875be0897949700ec2ee1779
jrsantos1 <jhonatanjonh@outlook.com>
2023-04-15 16:22:02 -0300

    refactor

--- src/main/java/br/com/projetopi/redesocial/service/TurmaService.java
+++ src/main/java/br/com/projetopi/redesocial/service/TurmaService.java
@@ -1,2 +1,35 @@
-package br.com.projetopi.redesocial.service;public class TurmaService {
+package br.com.projetopi.redesocial.service;
+
+import br.com.projetopi.redesocial.dao.TurmaDao;
+import br.com.projetopi.redesocial.interfaces.Service;
+import br.com.projetopi.redesocial.model.Turma;
+
+import java.util.ArrayList;
+
+public class TurmaService implements Service<Turma> {
+
+    private TurmaDao turmaDao;
+    public TurmaService(){
+        this.turmaDao = new TurmaDao();
+    }
+
+    @Override
+    public void add(Turma turma) {
+        turmaDao.add(turma);
+    }
+
+    @Override
+    public boolean update(Turma turma) {
+        return turmaDao.update(turma);
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return this.remove(id);
+    }
+
+    @Override
+    public ArrayList findAllPageable(int qtd_elementos, int num_inicio) {
+        return this.turmaDao.findAllPageable(qtd_elementos, num_inicio);
+    }
 }
