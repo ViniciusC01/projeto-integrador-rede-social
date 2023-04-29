95b35518b73d1fb4d3ff64574e3451b5e3ea7a00
Murilolima98 <murilolimaa1998@gmail.com>
2023-04-28 21:06:12 -0300

    testeusuario

--- src/main/java/br/com/projetopi/redesocial/service/UsuarioService.java
+++ src/main/java/br/com/projetopi/redesocial/service/UsuarioService.java
@@ -1,9 +1,12 @@
 package br.com.projetopi.redesocial.service;
 
 import br.com.projetopi.redesocial.dao.UsuarioDao;
+import br.com.projetopi.redesocial.interfaces.Service;
 import br.com.projetopi.redesocial.model.Usuario;
 
-public class UsuarioService {
+import java.util.ArrayList;
+
+public class UsuarioService implements Service<Usuario> {
 
     UsuarioDao usuarioDao;
 
@@ -14,6 +17,22 @@ public class UsuarioService {
     public void add(Usuario usuario){
         this.usuarioDao.add(usuario);
     }
+
+    @Override
+    public boolean update(Usuario object) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return false;
+    }
+
+    @Override
+    public ArrayList findAllPageable(int qtd_elementos, int num_inicio) {
+        return null;
+    }
+
 
     public int getIdByEmail(String email){
         int id = usuarioDao.getIdByEmail(email);
