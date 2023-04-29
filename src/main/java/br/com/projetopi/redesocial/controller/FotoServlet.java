2b71200c118ff126dda6c142e36c32d2d5e1f63b
ViniciusC01 <viniciuscarvalho134@gmail.com>
2023-04-28 21:58:22 -0300

    refactor FotoServlet

--- src/main/java/br/com/projetopi/redesocial/controller/FotoServlet.java
+++ src/main/java/br/com/projetopi/redesocial/controller/FotoServlet.java
@@ -2,8 +2,11 @@ package br.com.projetopi.redesocial.controller;
 
 import br.com.projetopi.redesocial.dao.FotoDAO;
 import br.com.projetopi.redesocial.model.Foto;
+import br.com.projetopi.redesocial.service.FotoService;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -12,72 +15,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-
-@WebServlet("/exemplo-servlet")
+@WebServlet("/foto")
 @MultipartConfig
-public class TesteServlet extends HttpServlet {
-
+public class FotoServlet extends HttpServlet {
+    private FotoService fotoService;
     private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        PrintWriter out = resp.getWriter();
-
-        req.getRequestDispatcher("teste.html").forward(req, resp);
+    public FotoServlet() {
+        this.fotoService = new FotoService();
     }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
+        String id = req.getParameter("id_get");
+        Optional<Foto> ft = new FotoDAO().findById(Integer.parseInt(id));
 
+        if (ft.isPresent()) {
+            byte[] dados = ft.get().getCd_foto();
+            resp.setContentType("image/jpeg");
+            resp.getOutputStream().write(ft.get().getCd_foto());
+        } else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String metodo = req.getParameter("_method");
-        if (metodo != null && metodo.equalsIgnoreCase("delete")) {
-            doDelete(req, resp);
-            return;
-        }
-        if (metodo != null && metodo.equalsIgnoreCase("put")) {
-            doPut(req, resp);
-            return;
-        }
-        if (metodo != null && metodo.equalsIgnoreCase("uploadimg")) {
-            service(req, resp);
-            return;
-        }
-
 
         Part img = req.getPart("img");
-        String id = req.getParameter("id");
         InputStream dadosEmBytes = img.getInputStream();
         byte [] dados = dadosEmBytes.readAllBytes();
-        Foto ft = new Foto();
-        ft.setId(Integer.parseInt(id));
-        ft.setCd_foto(dados);
-        new FotoDAO().toadd(ft);
-
-        req.getRequestDispatcher("teste.html").forward(req, resp);
+        fotoService.add(new Foto(dados));
     }
-
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id_delete");
-//        System.out.println("ID:" + id);
-        Foto foto = new Foto();
-        foto.setId(Integer.parseInt(id));
-        new FotoDAO().remove(foto);
-
-        req.getRequestDispatcher("teste.html").forward(req,resp);
+        new FotoDAO().remove(new Foto(Integer.parseInt(id),null));
     }
-
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id_put");
         Part img = req.getPart("img");
-        InputStream dadosEmBytes = req.getInputStream();
+        String id = req.getParameter("id_put");
+
+        InputStream dadosEmBytes = img.getInputStream();
         byte[] dados = dadosEmBytes.readAllBytes();
-
-        Foto foto = new Foto();
-        foto.setId(Integer.parseInt(id));
-        foto.setCd_foto(dados);
-        new FotoDAO().update(foto);
-
-        req.getRequestDispatcher("teste.html").forward(req,resp);
+        fotoService.update(new Foto(Integer.parseInt(id),dados));
     }
 }
